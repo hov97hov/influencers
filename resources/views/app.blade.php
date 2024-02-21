7ec1584c01de13891c14d6c0596066e43e4980ea
grig_1969ss <ggrig93@gmail.com>
2024-02-21 16:25:42 +0400

    bgfix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet"></link>
     <script src="{{ mix('/js/app.js') }}" defer></script>
     <link href="{{asset('/css/main.css')}}" rel="stylesheet"></link>
